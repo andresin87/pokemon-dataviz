f2af7ff100b5b15fdeea5936c693eb7d0d03687b
Andrés Lucas Enciso <andresin87@gmail.com>
2016-09-28 23:14:04 +0200

    ADD first fetch api call

--- dataFetch.js
+++ dataFetch.js
@@ -1,1 +1,26 @@
 // dataFetch.js
+
+const  fetch = require('node-fetch');
+
+const api = {
+    protocol: {
+        http: 'http://',
+        https: 'https://',
+    },
+    url: 'pokeapi.co/',
+    path: 'api/',
+    version: {
+        v1: 'v1',
+        v2: 'v2',
+    },
+};
+
+const apiqry = `${api.protocol.http}${api.url}${api.path}${api.version.v2}/`;
+
+
+fetch(`${apiqry}pokemon/?limit=1`)
+.then(function(res) {
+    return res.json();
+}).then(function(json) {
+    console.log(json);
+});
